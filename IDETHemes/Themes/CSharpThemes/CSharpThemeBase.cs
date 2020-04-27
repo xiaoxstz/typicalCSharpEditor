bf25beccfc7fb6c70d262de38893d4c9a47396e9
typicalHuman <reallytvold@gmai.com>
2020-04-27 14:33:18 +0600

    vs 2019 theme

---
+++ IDETHemes/Themes/CSharpThemes/CSharpThemeBase.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Media;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using IDEThemes.Themes.Interfaces;
+
+namespace IDEThemes.Themes.CSharpThemes
+{
+    public abstract class CSharpThemeBase: ITheme
+    {
+        #region InterfaceProperties
+        public IList<HighlightingColor> Colors { get; protected set; } = new List<HighlightingColor>();
+        public HighlightingRuleSet RuleSet { get; }
+        public Brush Background { get; protected set; }
+        public Brush Foreground { get; protected set; }
+        public HighlightingColor LineNumbersForeground { get; protected set; }
+        public Brush Selection { get; protected set; }
+        public HighlightingColor Hyperlink { get; protected set; }
+        public HighlightingColor NonPrintabelCharacters { get; protected set; }
+        #endregion
+
+        protected IHighlightingDefinition definition { get; set; }
+
+        public CSharpThemeBase()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string xmlCSharpFile = assembly.GetManifestResourceNames()
+                .First(fp => fp.Contains("CSharpDarkThemeColors.xshd"));
+            definition = GetDefiniion(xmlCSharpFile, assembly);
+            RuleSet = definition.MainRuleSet;
+            Colors = definition.NamedHighlightingColors.ToList();
+        }
+
+        public IHighlightingDefinition GetTheme()
+        {
+            SetDefinitionColors(definition);
+            SetDefinitionSpans(definition);
+            return definition;
+        }
+        private void SetDefinitionSpans(IHighlightingDefinition definition)
+        {
+            List<HighlightingSpan> spans = definition.MainRuleSet.Spans.ToList();
+            spans.Clear();
+            spans.AddRange(RuleSet.Spans);
+        }
+        private void SetDefinitionColors(IHighlightingDefinition definition)
+        {
+            List<HighlightingRule> rules = definition.MainRuleSet.Rules.ToList();
+            rules.Clear();
+            rules.ToList().AddRange(RuleSet.Rules);
+        }
+
+        protected IHighlightingDefinition GetDefiniion(string xmlCSharpFile, Assembly assembly)
+        {
+            using (var stream = assembly.GetManifestResourceStream(xmlCSharpFile))
+            {
+                using (var reader = new XmlTextReader(stream))
+                {
+                    return HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+        }
+
+
+    }
+}
