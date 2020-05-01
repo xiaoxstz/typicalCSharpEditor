f99401bda87e365ad0e5f95cad40029d9c605063
typicalHuman <reallytvold@gmai.com>
2020-05-01 14:17:59 +0600

    csharp light theme

---
+++ IDETHemes/Themes/CSharpThemes/LightTheme.cs
@@ -0,0 +1,14 @@
+﻿using IDEThemes.Themes.CSharpThemes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEThemes.Themes.CSharpThemes
+{
+    public class LightTheme: CSharpThemeBase
+    {
+        public override string FilePath { get; set; } = "CSharpLightTheme.xshd";
+    }
+}
---
+++ IDETHemes/Themes/CheckFiles/CSharpCheckFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDETHemes.Themes.CheckFiles
+{
+    public class CSharpCheckFile
+    {
+
+        public CSharpCheckFile()
+        {
+            char a = 'd';
+            string s = "str";
+            Console.WriteLine();
+        }
+        int GetNumber()
+        {
+            return default(int);
+        }
+
+
+        private int Number { get; set; }
+    }
+
+    interface Interface
+    {
+
+    }
+
+    struct Struct
+    {
+
+    }
+
+    enum Enum
+    {
+
+    }
+}
