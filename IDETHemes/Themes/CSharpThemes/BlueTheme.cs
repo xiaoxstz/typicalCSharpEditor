21d0267b97982b5cc7e50bf7823845b7e874ddf2
typicalHuman <reallytvold@gmai.com>
2020-05-01 12:15:11 +0600

    csharp blue theme

---
+++ IDETHemes/Themes/CSharpThemes/BlueTheme.cs
@@ -0,0 +1,7 @@
+﻿namespace IDEThemes.Themes.CSharpThemes
+{
+    public class BlueTheme: CSharpThemeBase
+    {
+        public override string FilePath { get; set; } = "CSharpBlueTheme.xshd";
+    }
+}
