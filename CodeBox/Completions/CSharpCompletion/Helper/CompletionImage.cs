0a565dc63b67d9b1f38a63108f8aaeb012e153f1
typicalHuman <reallytovald@gmail.com>
2020-05-13 17:33:37 +0600

    transferring images to resources

--- CodeBox/Completions/CSharpCompletion/Helper/CompletionImage.cs
+++ CodeBox/Completions/CSharpCompletion/Helper/CompletionImage.cs
@@ -1,19 +1,57 @@
 ﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using CodeBox.Properties;
 
 namespace Completions.CSharpCompletion
 {
     internal class CompletionImage
     {
-        private const string IMAGES_PATH = @"\Completions\CSharpCompletion\Images\";
-        private const string PNG_EXTENSION = ".png";
-
         internal static ImageSource GetImageSource(CompletionTypes type)
         {
-            string completionImagePath = $"{IMAGES_PATH}{type.ToString()}{PNG_EXTENSION}";
-            Uri uri = new Uri(completionImagePath, UriKind.Relative);
-            return new BitmapImage(uri);
+            switch(type)
+            {
+                case CompletionTypes.Class:
+                    return ImageSourceFromBitmap(Resources.Class);
+                case CompletionTypes.Delegate:
+                    return ImageSourceFromBitmap(Resources.Delegate);
+                case CompletionTypes.Enum:
+                    return ImageSourceFromBitmap(Resources.Enum);
+                case CompletionTypes.Field:
+                    return ImageSourceFromBitmap(Resources.Field);
+                case CompletionTypes.Interface:
+                    return ImageSourceFromBitmap(Resources.Interface);
+                case CompletionTypes.Keyword:
+                    return ImageSourceFromBitmap(Resources.Keyword);
+                case CompletionTypes.Namespace:
+                    return ImageSourceFromBitmap(Resources.Namespace);
+                case CompletionTypes.Property:
+                    return ImageSourceFromBitmap(Resources.Property);
+                case CompletionTypes.Snippet:
+                    return ImageSourceFromBitmap(Resources.Snippet);
+                case CompletionTypes.Structure:
+                    return ImageSourceFromBitmap(Resources.Struct);
+                default:
+                    throw new Exception("Incorrect type");
+            }
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteObject([In] IntPtr hObject);
+
+        private static ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
         }
     }
 }
